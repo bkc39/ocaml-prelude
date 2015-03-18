2a079a74d3feb2ebd77506d92e5ce88b7ba11395
bkc39 <bkc39@cornell.edu>
2015-03-17 23:26:37 -0400

    Fix bugs in implementation of void and infix operators.

--- control/functor.ml
+++ control/functor.ml
@@ -5,36 +5,28 @@ module type FUNCTOR_DEF = sig
     val map : ('a -> 'b) -> 'a t -> 'b t
   end
 
-(* module type FUNCTOR_INFIX = sig *)
-(*   type 'a t *)
-(*   val ( |> )  : 'a t -> ('a -> 'b) -> 'b t *)
-(*   val ( <$> ) : ('a -> 'b) -> 'a t -> 'b t *)
-(*   val ( <$ )  : 'a -> 'b t -> 'a t *)
-(*   val ( $> )  : 'a -> 'b -> 'c t -> 'b t *)
-(* end *)
-
 module type S = sig
     type 'a t
     val map  : ('a -> 'b) -> 'a t -> 'b t
-    val void : 'a -> 'b t -> unit t
+    val void : 'a t -> unit t
     module FunctorInfix :
     sig
       val ( |> )  : 'a t -> ('a -> 'b) -> 'b t
       val ( <$> ) : ('a -> 'b) -> 'a t -> 'b t
       val ( <$ )  : 'a -> 'b t -> 'a t
-      val ( $> )  : 'a -> 'b -> 'c t -> 'b t
+      val ( $> )  : 'a t -> 'b -> 'b t
     end
   end
 
 module MakeFunctor (F : FUNCTOR_DEF) : (S with type 'a t := 'a F.t) = struct
     include F
 
-    let void _ = map (const ())
+    let void f = map (const ()) f
 
     module FunctorInfix = struct
         let ( |> ) x f = map f x
         let ( <$> )    = map
         let ( <$ ) x   = map (const x)
-        let ( $> ) _ x = map (const x)
+        let ( $> ) y x = map (const x) y
       end
   end
